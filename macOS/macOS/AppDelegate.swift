b578f74c20fd39e6052f4e4a49a155dab2ab6f93
madordie <keith_127@126.com>
2017-11-03 11:47:59 +0800

    first commit

---
+++ macOS/macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS
+//
+//  Created by 孙继刚 on 2017/11/3.
+//  Copyright © 2017年 madordie.github.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS/macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS
+//
+//  Created by 孙继刚 on 2017/11/3.
+//  Copyright © 2017年 madordie.github.io. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
